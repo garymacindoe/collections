9a2679ba06cb10fc68e46bd774f8bd4d8c76b5f4
Gary Macindoe <gary@garymacindoe.co.uk>
2015-09-07 22:02:46 +0100

    Prototypes for insertion, selection and merge sort

---
+++ include/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+void insertion_sort(void *, size_t, size_t, int (*)(const void *, const void *));
+void selection_sort(void *, size_t, size_t, int (*)(const void *, const void *));
+void merge_sort(void *, size_t, size_t, int (*)(const void *, const void *), void *);
+
+#endif
